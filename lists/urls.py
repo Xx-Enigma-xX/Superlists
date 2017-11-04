de82c208e75138987ffe245eff44f47eed03609f
Paul Lee <hillcrestpaul0719@gmail.com>
2017-11-03 23:01:26 -0700

    Minimum Viable Site Finally!

--- lists/urls.py
+++ lists/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 #from django.conf.urls import url
 #from django.contrib import admin
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 # urlpatterns = patterns("",
@@ -24,7 +24,7 @@ from django.contrib import admin
 # )
 
 urlpatterns = [
-	url(r'^$', 'lists.views.home_page', name='home'),
-	url(r'^lists/', include('lists.urls')),
-	# url(r'^admin/', include(admin.site.urls)),
+	url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+	url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+	url(r'^new$', 'lists.views.new_list', name='new_list'),
 ]
