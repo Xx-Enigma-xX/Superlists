f77a7d47b7c6f6291ce50bbcc9a09d5fba3bc7c4
Paul Lee <hillcrestpaul0719@gmail.com>
2017-11-03 21:19:50 -0700

    changes to the database, now with a parent list and more fun

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 # home_page = None
 
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
 	return render(request, 'home.html')
@@ -17,5 +17,6 @@ def view_list(request):
 	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
